044a14b6b77148323f76a17b099bd59782a7c87b
Cornelius Schuchardt <cornelius.schuchardt@beck.de>
2024-11-17 19:53:03 +0000

    fix

--- pulumi/src/pulumi/main.py
+++ pulumi/src/pulumi/main.py
@@ -9,39 +9,38 @@ class Pulumi:
     """Pulumi Functions for Azure Configurations"""
     
     storage_account_name: Annotated[str, Doc("The name of the Azure Storage Account for state storage")] = field(default="")
+    container_name: Annotated[str, Doc("The name of the Azure Blob Container for state storage")] = field(default="")
+    config_passphrase: Annotated[dagger.Secret, Doc("The passphrase for the Pulumi configuration")] = field(default=dagger.Secret("pulumi"))
+    infrastructure_path: Annotated[dagger.Directory, Doc("The path to the Pulumi infrastructure code")] = field(default=dagger.Directory("/infra")) 
+    stack_name: Annotated[str, Doc("The name of the Pulumi stack to use")] = field(default="dev")
+    config_passphrase: Annotated[dagger.Secret, Doc("The passphrase for the Pulumi configuration")] = field(default=dagger.Secret("pulumi"))
+    ctr: dagger.Container = dag.container().from_("pulumi/pulumi:latest")
     
-    async def test_stack(self, container: dagger.Container, stack_name: str) -> bool:
+    async def test_stack(self) -> bool:
         """Query all existing stacks in the Pulumi state file"""
-        stack_output = await container.with_exec(["pulumi", "stack", "ls", "--json"]).stdout()
+        stack_output = await self.ctr.with_exec(["pulumi", "stack", "ls", "--json"]).stdout()
         stacks = json.loads(stack_output)
-        return any(stack.get("name") == stack_name for stack in stacks)
+        return any(stack.get("name") == self.stack_name for stack in stacks)
 
     @function
     async def create_or_select_stack(
         self,
-        container_name: str,
-        config_passphrase: dagger.Secret,
-        infrastructure_path: dagger.Directory,
-        stack_name: str,
         azure_cli_path: dagger.Directory | None,
         azure_oidc_token: str | None,
         azure_client_id: str | None, 
         azure_tenant_id: str | None, 
     ) -> dagger.Container:
         """Create or select a stack in the Pulumi state file"""
-        ctr = self.pulumi_az_base(
-            container_name=container_name,
-            config_passphrase=config_passphrase,
-            infrastructure_path=infrastructure_path,
+        self.setup_azure_authentication(
             azure_cli_path=azure_cli_path,
             azure_oidc_token=azure_oidc_token,
             azure_client_id=azure_client_id,
             azure_tenant_id=azure_tenant_id
         )
-        if not await self.test_stack(ctr, stack_name):
-            return await ctr.with_exec(["pulumi", "stack", "init", stack_name])
+        if not await self.test_stack(self.stack_name):
+            return await self.ctr.with_exec(["pulumi", "stack", "init", self.stack_name])
         else:
-            return await ctr.with_exec(["pulumi", "stack", "select", stack_name])    
+            return await self.ctr.with_exec(["pulumi", "stack", "select", self.stack_name])    
     
     @function
     async def preview(
@@ -60,9 +59,9 @@ class Pulumi:
         
         # Setup class attributes 
         self.storage_account_name = storage_account_name
+        self.container_name = container_name
         
         ctr = await self.create_or_select_stack(
-            container_name=container_name,
             config_passphrase=config_passphrase,
             infrastructure_path=infrastructure_path,
             stack_name=stack_name,
@@ -93,30 +92,25 @@ class Pulumi:
         
         # Setup class attributes 
         self.storage_account_name = storage_account_name
-        
+        self.container_name = container_name
+        self.config_passphrase = config_passphrase
+        self.infrastructure_path = infrastructure_path
+        self.stack_name = stack_name
         
         ctr = await self.create_or_select_stack(
-            container_name=container_name,
-            config_passphrase=config_passphrase,
-            infrastructure_path=infrastructure_path,
-            stack_name=stack_name,
             azure_cli_path=azure_cli_path,
             azure_oidc_token=azure_oidc_token,
             azure_client_id=azure_client_id,
             azure_tenant_id=azure_tenant_id
         )
-        # return await (
-        #     ctr.with_env_variable("PULUMI_EXPERIMENTAL", "true") \
-        #     .with_exec(["pulumi", "preview","--save-plan","plan.json"]) \
-        #     .file("/infra/plan.json")            
-        # )
+        
         return await (
             ctr.with_exec(["/bin/sh", "-c", "`pulumi preview > plan.json`"]) \
             .file("/infra/plan.json")            
         )
     
     @function
-    async def debug_env(
+    async def terminal(
         self,
         storage_account_name: str,
         container_name: str,
@@ -130,16 +124,20 @@ class Pulumi:
     ) -> dagger.Container:
         """Preview the changes to the infrastructure"""
         
+        # Setup class attributes 
+        self.storage_account_name = storage_account_name
+        self.container_name = container_name
+        self.config_passphrase = config_passphrase
+        self.infrastructure_path = infrastructure_path
+        self.stack_name = stack_name
+        
         ctr = await self.create_or_select_stack(
-            container_name=container_name,
-            config_passphrase=config_passphrase,
-            infrastructure_path=infrastructure_path,
-            stack_name=stack_name,
             azure_cli_path=azure_cli_path,
             azure_oidc_token=azure_oidc_token,
             azure_client_id=azure_client_id,
-            azure_tenant_id=azure_tenant_id, 
+            azure_tenant_id=azure_tenant_id
         )
+        
         return await (
             ctr.terminal()
         )
@@ -161,46 +159,43 @@ class Pulumi:
         
         # Setup class attributes 
         self.storage_account_name = storage_account_name
+        self.container_name = container_name
+        self.config_passphrase = config_passphrase
+        self.infrastructure_path = infrastructure_path
+        self.stack_name = stack_name
         
         ctr = await self.create_or_select_stack(
-            container_name=container_name,
-            config_passphrase=config_passphrase,
-            infrastructure_path=infrastructure_path,
-            stack_name=stack_name,
             azure_cli_path=azure_cli_path,
             azure_oidc_token=azure_oidc_token,
             azure_client_id=azure_client_id,
-            azure_tenant_id=azure_tenant_id, 
+            azure_tenant_id=azure_tenant_id
         )
+        
         return await (
             # ctr.with_exec(["pip", "install", "-r", "requirements.txt"])
             ctr.with_exec(["pulumi", "up","-f"]).stdout()
         )
 
-    def pulumi_az_base(
+    def setup_azure_authentication(
         self,
-        container_name: str,
-        config_passphrase: dagger.Secret,
-        infrastructure_path: dagger.Directory,
         azure_cli_path: dagger.Directory | None,
         azure_oidc_token: str | None,
         azure_client_id: str | None, 
         azure_tenant_id: str | None, 
-    ) -> dagger.Container:
+    ) -> None:
         """Returns Pulumi container with Azure Authentication"""
         blob_address = (
-            f"azblob://{container_name}?storage_account={self.storage_account_name}"
+            f"azblob://{self.container_name}?storage_account={self.storage_account_name}"
         )
-        filtered_source = infrastructure_path.without_directory("venv")
-        ctr = dag.container().from_("pulumi/pulumi:latest")
+        filtered_source = self.infrastructure_path.without_directory("venv")
         
         if azure_cli_path:
-            ctr = ctr.with_directory("/root/.azure", azure_cli_path)\
+            self.ctr.with_directory("/root/.azure", azure_cli_path)\
                 .with_env_variable("AZURE_AUTH", "az")
         
         if azure_oidc_token:
             oidc_token_path = "/root/.azure/oidc_token"
-            ctr = ctr.with_new_file(oidc_token_path, azure_oidc_token) \
+            self.ctr.with_new_file(oidc_token_path, azure_oidc_token) \
                 .with_env_variable("ARM_OIDC_TOKEN", azure_oidc_token) \
                 .with_env_variable("AZURE_OIDC_TOKEN", azure_oidc_token) \
                 .with_env_variable("ARM_USE_OIDC", "true") \
@@ -211,10 +206,8 @@ class Pulumi:
                 .with_env_variable("AZURE_TENANT_ID", azure_tenant_id) \
                 .with_env_variable("AZURE_FEDERATED_TOKEN_FILE", oidc_token_path)
         
-        ctr = ctr \
-            .with_secret_variable("PULUMI_CONFIG_PASSPHRASE", config_passphrase) \
+        self.ctr \
+            .with_secret_variable("PULUMI_CONFIG_PASSPHRASE", self.config_passphrase) \
             .with_directory("/infra", filtered_source) \
             .with_workdir("/infra") \
             .with_exec(["pulumi", "login", blob_address])
-        
-        return ctr
